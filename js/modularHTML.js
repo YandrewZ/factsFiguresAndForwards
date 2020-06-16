f0bea5612b48854be02c7c2a6a4a99ef99312085
maxfer1221 <63942620+maxfer1221@users.noreply.github.com>
2020-06-15 20:20:38 -0400

    Update modularHTML.js

--- js/modularHTML.js
+++ js/modularHTML.js
@@ -50,7 +50,7 @@ function extendFile() {
 
                 turn_right = !turn_right;
                 tempCard.getElementsByClassName("article-title")[0].innerText = `View ${articlesByDate[universal_date_index].length - 2} more links shared on ${Months[tempDate.getMonth()]} ${getDayAsStr(tempDate.getDate())}`;
-                tempCard.getElementsByClassName("article-title")[0].onclick = displayList(getVal(cached_date_index), tempDate);
+                tempCard.getElementsByClassName("article-title")[0].onclick = displayList(cached_date_index, tempDate);
                 tempCard.getElementsByClassName("article-title")[0].href = 'extra_html/link_list.html';
                 tempCard.getElementsByClassName("introductory-snippet")[0].innerText = `${articlesByDate[universal_date_index][2].link}\n${articlesByDate[universal_date_index][3].link}\n...`;
                 tempCard.getElementsByClassName("source-url")[0].innerText = '';
